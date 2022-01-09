49f73d07e9a57cb95bf09ffa47a2eb544d2bd598
Arsenii Liunsha <me@arslee.dev>
2022-01-09 22:05:08 +0900

    feat(routes): Implement self fetching

--- users_microservice/api/routes.py
+++ users_microservice/api/routes.py
@@ -1,4 +1,5 @@
 from fastapi.routing import APIRouter
+from fastapi import Depends
 from users_microservice.api.models import User
 from users_microservice.api.discord import discord
 
@@ -16,6 +17,11 @@ async def auth_callback(code: str):
     token, _ = await discord.get_access_token(code)
     user_data = await discord.request("/users/@me", token)
     return await User.objects.get_or_create(discord_id=user_data["id"])
+
+
+@users.get("/me")
+async def get_self(user: User = Depends(discord.user)):
+    return await User.objects.get(discord_id=user.id)
 
 
 @users.get("/{id}", response_model=User)
