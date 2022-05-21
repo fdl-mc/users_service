b70e737992e75dff4013e5c43f29b9d462e9ee73
arslee <me@arslee.dev>
2022-05-21 09:45:37 +0900

    chore(proto): Reponse -> Reply

--- src/service.rs
+++ src/service.rs
@@ -3,9 +3,9 @@ use tonic::{Request, Response, Status};
 use crate::model::UserModel;
 use crate::users_proto::users_server::Users as UsersServiceTrait;
 use crate::users_proto::{
-    ChangePasswordRequest, ChangePasswordResponse, FindUsersRequest, FindUsersResponse,
-    GetAllUsersRequest, GetAllUsersResponse, GetSelfUserRequest, GetSelfUserResponse,
-    GetUserByIdRequest, GetUserByIdResponse, LoginRequest, LoginResponse,
+    ChangePasswordReply, ChangePasswordRequest, FindUsersReply, FindUsersRequest, GetAllUsersReply,
+    GetAllUsersRequest, GetSelfUserReply, GetSelfUserRequest, GetUserByIdReply, GetUserByIdRequest,
+    LoginReply, LoginRequest,
 };
 
 #[derive(Debug)]
@@ -18,11 +18,15 @@ impl UsersServiceTrait for UsersService {
     async fn get_all_users(
         &self,
         _request: Request<GetAllUsersRequest>,
-    ) -> Result<Response<GetAllUsersResponse>, Status> {
-        match UserModel::get_all(&self.pool.clone()).await {
-            Ok(res) => Ok(Response::new(GetAllUsersResponse {
-                users: res.iter().map(|x| x.into_message()).collect(),
-            })),
+    ) -> Result<Response<GetAllUsersReply>, Status> {
+        let res = UserModel::get_all(&self.pool.clone()).await;
+
+        match res {
+            Ok(res) => {
+                let users = res.iter().map(|user| user.into_message()).collect();
+                let reply = GetAllUsersReply { users };
+                Ok(Response::new(reply))
+            }
             Err(err) => Err(Status::internal(err.to_string())),
         }
     }
@@ -30,35 +34,32 @@ impl UsersServiceTrait for UsersService {
     async fn get_user_by_id(
         &self,
         request: Request<GetUserByIdRequest>,
-    ) -> Result<Response<GetUserByIdResponse>, Status> {
+    ) -> Result<Response<GetUserByIdReply>, Status> {
         unimplemented!()
     }
 
     async fn get_self_user(
         &self,
         request: Request<GetSelfUserRequest>,
-    ) -> Result<Response<GetSelfUserResponse>, Status> {
+    ) -> Result<Response<GetSelfUserReply>, Status> {
         unimplemented!()
     }
 
     async fn find_users(
         &self,
         request: Request<FindUsersRequest>,
-    ) -> Result<Response<FindUsersResponse>, Status> {
+    ) -> Result<Response<FindUsersReply>, Status> {
         unimplemented!()
     }
 
-    async fn login(
-        &self,
-        request: Request<LoginRequest>,
-    ) -> Result<Response<LoginResponse>, Status> {
+    async fn login(&self, request: Request<LoginRequest>) -> Result<Response<LoginReply>, Status> {
         unimplemented!()
     }
 
     async fn change_password(
         &self,
         request: Request<ChangePasswordRequest>,
-    ) -> Result<Response<ChangePasswordResponse>, Status> {
+    ) -> Result<Response<ChangePasswordReply>, Status> {
         unimplemented!()
     }
 }
