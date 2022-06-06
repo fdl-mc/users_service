e3d27a389785f1b0c5ff0e30fa8cc81d8350788a
arslee <mail@arslee.me>
2022-06-06 13:30:49 +0900

    chore: Change default port to 8000

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await
         .unwrap();
 
-    let addr = "[::1]:50051".parse().unwrap();
+    let addr = "[::1]:8000".parse().unwrap();
     let users_service = service::UsersService { pool, config };
 
     tracing::info!(message = "Starting server.", %addr);
